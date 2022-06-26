deb528330072eb921dc35b95d2a1f81b92a98e2c
MinHK4 <dzf4444@gmail.com>
2022-06-26 17:06:08 +0900

    PRG_DP_2 sol BottomUp sol added

--- PRG_DP_2.cpp
+++ PRG_DP_2.cpp
@@ -19,7 +19,7 @@ int dfs(int row, int col){
     return dp[row][col];
 }
 
-int solution(vector<vector<int>> triangle) {
+int Top_Down_solution(vector<vector<int>> triangle) {
     int answer = -1;
     
     org = triangle;
@@ -31,10 +31,37 @@ int solution(vector<vector<int>> triangle) {
     return answer;
 }
 
-/*
-DFS + Top-down 방식으로 풀었는데
-그냥 Bottom-Up 방식으로 푸는게 훨씬 더 간단하고 빨리 풀 수 있는거 같음
 
-dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + triangle[i+ 1][j]);
-dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + triangle[i+ 1][j + 1]);
-*/
+///////////////////////////////////////////////
+
+
+int Bottom_Up_solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    
+    // DP 배열 초기화
+    vector<vector<int>> dp(triangle.size());
+    for(int i = 0 ; i < triangle.size(); i++){
+        dp[i].resize(triangle[i].size());
+    }
+    dp[0][0] = triangle[0][0];
+    
+    // DP[i][j] 는 꼭대기부터 (i,j)까지 온 루트의 합의 최댓값
+    // DP[i][j] = tri[i][j] + max(DP[i-1][j], DP[i-1][j-1])
+    // 인덱싱만 조심해주기
+    for(int i = 1; i < triangle.size(); i++){
+        for(int j = 0; j < triangle[i].size(); j++){
+            int temp = 0;
+            if(0 <= j && j < dp[i-1].size())
+                temp = max(temp,dp[i-1][j]);
+            if(0 <= j-1 && j-1 < dp[i-1].size())
+                temp = max(temp,dp[i-1][j-1]);
+            dp[i][j] = triangle[i][j] + temp;
+        }
+    }
+    
+    // 맨 끝 열에서 가장 큰 값 찾아서 return
+    int N = triangle.size() - 1;
+    for(int j = 0; j < triangle[N].size(); j++)
+        answer = max(answer, dp[N][j]);
+    return answer;
+}
