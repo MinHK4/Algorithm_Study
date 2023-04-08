00801ee46d1d1c6a7504535532d286ddf9c06141
MinHK4 <dzf1224@naver.com>
2023-04-08 17:34:14 +0900

    sol 1 revised

--- SWEA_벽돌깨기.cpp
+++ SWEA_벽돌깨기.cpp
@@ -1,8 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <cstdio>
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <queue>
 
 using namespace std;
+
+int N, W, H;
+int ans;
+
+void display(vector<vector<int>> arr2D);
+void dfs(vector<vector<int>> arr, int count);
 
 int main(int argc, char** argv)
 {
@@ -17,19 +27,122 @@ int main(int argc, char** argv)
 	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
 	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
 	*/
-	freopen("sample_input.txt", "r", stdin);
+	freopen("input.txt", "r", stdin);
 
 	cin >> T;
-	for(test_case = 1; test_case <= T; ++test_case){
-    int N, W, H;
-    cin >> N >> W >> H;
+	for (test_case = 1; test_case <= T; ++test_case) {
+		cin >> N >> W >> H;
 
+		vector<vector<int>> arr(W, vector<int>(H));
+		for (int i = H - 1; i >= 0; i--) {
+			for (int j = 0; j < W; j++) {
+				cin >> arr[j][i];
+			}
+		}
 
+		ans = N * W * H;
+		
+		dfs(arr, 0);
 
-
-    cout << N << W << H;
-
-
+		cout << '#' << test_case << ' ' << ans << '\n';
 	}
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+
+	return 0;
+}
+
+bool isValidPos(int row, int col) {
+	return 0 <= row && row < W && 0 <= col && col < H;
+}
+
+// 블럭 뿌셔보기 돌린 배열 반화해주는 함수
+vector<vector<int>> simulate(vector<vector<int>> arr, int row, int col) {
+	
+	//cout << "============BEFORE==========\n";
+	//display(arr);
+
+	if (!isValidPos(row, col)) return arr;
+	
+	queue <pair<int, int>> q;
+	q.push({ row, col });
+
+	while (!q.empty()) {
+		int r = q.front().first;
+		int c = q.front().second;
+		int num = arr[r][c];
+		q.pop();
+
+		//cout << r << ' ' << c << ' ' << num << '\n';
+
+		arr[r][c] = 0;
+		num--;
+
+		int d = 1;
+		while (d <= num) {
+			if (isValidPos(r - d, c)) q.push({ r - d, c });
+			if (isValidPos(r + d, c)) q.push({ r + d, c });
+			if (isValidPos(r, c - d)) q.push({ r, c - d });
+			if (isValidPos(r, c + d)) q.push({ r, c + d});
+			d++;
+		}
+	}
+
+	vector<vector<int>> new_arr;
+	for (int i = 0; i < W; i++) {
+		vector<int> temp;
+		for (auto x: arr[i]) {
+			if (x != 0) temp.push_back(x);
+		}
+
+		while (temp.size() < H) {
+			temp.push_back(0);
+		}
+
+		new_arr.push_back(temp);
+	}
+
+
+	//cout << "============AFTER==========\n";
+	//display(new_arr);
+
+	return new_arr;
+}
+
+// 모든 경우 탐색하는 dfs 함수
+void dfs(vector<vector<int>> arr, int count) {
+	
+	// 최종적으로 남아있는 벽돌 개수 카운트해서 가장 적게 남은 경우 찾아주기
+	if (count == N) {
+		int blocks = 0;
+		for (auto x : arr) {
+			for (auto y : x) {
+				if (y != 0) blocks++;
+			}
+		}
+		ans = min(ans, blocks);
+
+		return;
+	}
+
+	for (int i = 0; i < W; i++) {
+		int j;
+		for (j = H - 1; j >= 0; j--) {
+			if (arr[i][j] != 0) break;
+		}
+		
+		//cout << count <<"\n\n";
+
+		vector<vector<int>> temp = simulate(arr, i, j);
+		dfs(temp, count + 1);
+	}
+}
+
+// 출력용 함수
+void display(vector<vector<int>> arr2D) {
+	for (auto arr : arr2D) {
+		for (auto x : arr) {
+			cout << x << ' ';
+		}
+		cout << '\n';
+	}
+	cout << '\n';
 }
